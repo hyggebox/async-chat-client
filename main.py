35f01b34a022b55b066f271957649eb26dd4b8b6
hyggebox <asya_apto@mail.ru>
2022-09-26 17:46:20 +0300

    Add CLI options

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+import argparse
 import asyncio
 import datetime
+import os
 
 import aiofiles
 
 
-async def chat_reader():
-    reader, writer = await asyncio.open_connection(
-        'minechat.dvmn.org', 5000)
+async def chat_reader(host, port, history_file):
+    reader, writer = await asyncio.open_connection(host, port)
 
     while True:
         line = await reader.readline()
@@ -17,10 +18,21 @@ async def chat_reader():
         message = f'[{now}] {line.decode()}'
         print(message.rstrip())
 
-        async with aiofiles.open('chat.txt', mode='a') as f:
-            contents = await f.write(message)
+        async with aiofiles.open(history_file, mode='a') as f:
+            await f.write(message)
 
     writer.close()
 
 
-asyncio.run(chat_reader())
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', default='minechat.dvmn.org')
+    parser.add_argument('--port', type=int, default=5000)
+    parser.add_argument('--history_file', '-f', default='chat.txt')
+    args = parser.parse_args()
+
+    host = args.host or os.getenv('HOST')
+    port = args.port or os.getenv('PORT')
+    history_file = args.history_file or os.getenv('HISTORY_FILE')
+
+    asyncio.run(chat_reader(host, port, history_file))
