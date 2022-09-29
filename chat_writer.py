333dd244e27b9ee12fd0d69669cf3dcb6a1fbbf3
hyggebox <asya_apto@mail.ru>
2022-09-29 10:39:11 +0300

    Add new script to send messages.

---
+++ chat_writer.py
@@ -0,0 +1,34 @@
+import argparse
+import asyncio
+import os
+
+TOKEN = '3d9a78ce-3daa-11ed-8c47-0242ac110002'
+
+async def chat_writer(host, port, message):
+    reader, writer = await asyncio.open_connection(host, port)
+    reply = await reader.readline()
+    print(reply)
+    writer.write((TOKEN + '\n').encode())
+    await writer.drain()
+    reply = await reader.readline()
+    print(reply)
+    writer.write((message + '\n\n').encode())
+    await writer.drain()
+    reply = await reader.readline()
+    print(reply)
+
+    writer.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', default='minechat.dvmn.org')
+    parser.add_argument('--port', type=int, default=5050)
+    parser.add_argument('--msg')
+    args = parser.parse_args()
+
+    host = args.host or os.getenv('HOST')
+    port = args.port or os.getenv('PORT')
+    msg = args.msg
+
+    asyncio.run(chat_writer(host, port, msg))
