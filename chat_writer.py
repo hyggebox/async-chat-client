07646ac87cb2871bd1fe3d26f84702328c46d7ca
hyggebox <asya_apto@mail.ru>
2022-09-29 10:51:07 +0300

    Add logging

--- chat_writer.py
+++ chat_writer.py
@@ -1,5 +1,6 @@
 import argparse
 import asyncio
+import logging
 import os
 
 TOKEN = '3d9a78ce-3daa-11ed-8c47-0242ac110002'
@@ -7,20 +8,22 @@ TOKEN = '3d9a78ce-3daa-11ed-8c47-0242ac110002'
 async def chat_writer(host, port, message):
     reader, writer = await asyncio.open_connection(host, port)
     reply = await reader.readline()
-    print(reply)
+    logging.debug(reply)
     writer.write((TOKEN + '\n').encode())
     await writer.drain()
     reply = await reader.readline()
-    print(reply)
+    logging.debug(reply)
     writer.write((message + '\n\n').encode())
     await writer.drain()
     reply = await reader.readline()
-    print(reply)
+    logging.debug(reply)
 
     writer.close()
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', default='minechat.dvmn.org')
     parser.add_argument('--port', type=int, default=5050)
