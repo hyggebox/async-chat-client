129bc940b125774ec77e501cc99599314495754f
hyggebox <asya_apto@mail.ru>
2022-10-01 16:08:18 +0300

    Refactor code, split by reusable functions

--- chat_writer.py
+++ chat_writer.py
@@ -6,28 +6,19 @@ import os
 
 import aiofiles
 
-TOKEN = '3d9a78ce-3daa-11ed-8c47-0242ac110002'
 
-async def chat_writer(host, port, message):
-    reader, writer = await asyncio.open_connection(host, port)
+async def authorize_user(reader, writer, account_hash):
     reply = await reader.readline()
     logging.debug(reply)
-    writer.write((TOKEN + '\n').encode())
+    writer.write((account_hash + '\n').encode())
     await writer.drain()
     reply = await reader.readline()
     logging.debug(reply)
     if not json.loads(reply):
         print('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
 
-    writer.write((message + '\n\n').encode())
-    await writer.drain()
-    reply = await reader.readline()
-    logging.debug(reply)
 
-    writer.close()
-
-
-async def register_user(host, port, nickname):
+async def register_user(reader, writer, nickname):
     reader, writer = await asyncio.open_connection(host, port)
     reply = await reader.readline()
     logging.debug(reply)
@@ -47,17 +38,44 @@ async def register_user(host, port, nickname):
     writer.close()
 
 
+async def submit_message(reader, writer, message):
+    writer.write((message + '\n\n').encode())
+    await writer.drain()
+    reply = await reader.readline()
+    logging.debug(reply)
+
+
+async def run_chat(host, port, message, nickname):
+    reader, writer = await asyncio.open_connection(host, port)
+
+    if nickname:
+        await register_user(reader, writer, nickname)
+    elif os.path.exists('user_details.json'):
+        async with aiofiles.open('user_details.json', 'r') as f:
+            data = await f.read()
+            account_hash = json.loads(data)['account_hash']
+            await authorize_user(reader, writer, account_hash)
+    else:
+        print('Придумайте ник для регистрации в чате: --nickname / -n')
+        return
+
+    await submit_message(reader, writer, message)
+    writer.close()
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', default='minechat.dvmn.org')
     parser.add_argument('--port', type=int, default=5050)
-    parser.add_argument('--msg')
+    parser.add_argument('--message', '-m', default='Hello!')
+    parser.add_argument('--nickname', '-n')
     args = parser.parse_args()
 
     host = args.host or os.getenv('HOST')
     port = args.port or os.getenv('PORT')
-    msg = args.msg
+    msg = args.message
+    nickname = args.nickname or 0
 
-    asyncio.run(chat_writer(host, port, msg))
+    asyncio.run(run_chat(host, port, msg, nickname))
